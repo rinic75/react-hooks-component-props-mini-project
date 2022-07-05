b7a44a5ceb2fdcf22a9591cb8456d85e790e9044
Chang Shin <cshin7374@gmail.com>
2022-07-04 21:27:48 -0500

    commit tree 06eba7d12b1c0921b582078cd8384e8fac4c0c71

---
+++ src/components/articlelist.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Article from "./article";
+
+function ArticleList({posts}) {
+  const articlelists = posts.map(post => (
+    <Article 
+      key = {post.id}
+      title = {post.title}
+      date = {post.date}
+      preview = {post.preview}
+      minutes = {post.minutes}
+    />)
+  )
+
+  return (
+    <main>{articlelists}</main>
+  )
+}
+
+export default ArticleList
